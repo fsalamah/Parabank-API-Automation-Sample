13aa209013eaa9ca019637595b075c645ff0f275
fsalamah <36605682+fsalamah@users.noreply.github.com>
2024-04-19 12:32:00 +0400

    Add files via upload
    updated account keys

--- src/test/java/tests/TestTransfer.java
+++ src/test/java/tests/TestTransfer.java
@@ -25,85 +25,85 @@ public class TestTransfer extends BaseTest {
 
 	private static HashMap<String,String> userSessions = DataStore.getInstance().getUserSessions();
 	private static HashMap<String,Integer> customerAccounts = DataStore.getInstance().getCustomerAccounts();
-			
+
 	@BeforeClass
 	public void Setup() throws IOException {
 
 		// Clean up the database
 		AdminEndpoints.cleanDatabase();
-		
+
 		// initialize account balances since the test relies on these
-		AdminEndpoints.initializeAccountBalanceSettings();												
-		
-		//Setup the data for the tests (users and accounts) 
+		AdminEndpoints.initializeAccountBalanceSettings();
+
+		//Setup the data for the tests (users and accounts)
 		TestDataHelper.setupInitialTestData();
 
 	}
 
 
-	 
-	
-	
-	@Test( testName = "Verify Transfer Positive Cases", 
-			dataProvider 	= "test-data-provider", 
-			dataProviderClass = TestDataProvider.class, 
+
+
+
+	@Test( testName = "Verify Transfer Positive Cases",
+			dataProvider 	= "test-data-provider",
+			dataProviderClass = TestDataProvider.class,
 			priority = 0, groups = {"regression", "smoke"},
 			retryAnalyzer = RetryListener.class)
 	public void verifyTransferPositiveCases(Map<String, String> testData) {
-		
+
 		Allure.description(testData.get("description"));
-		
+
 	    AllureLifecycle lifecycle = Allure.getLifecycle();
 	    lifecycle.updateTestCase(testResult -> testResult.setName(testData.get("description")));
-	    
+
 		//get the user token
-		var sessionId = userSessions.get(testData.get("username"));		
-				
-		//get the source account id 
-		int sendAccountId = customerAccounts.get(getAccountKeyFromTestData(testData));
-		//get the source account id 
-		int receiveAccountId = customerAccounts.get(getAccountKeyFromTestData(testData));
-		
-		
+		var sessionId = userSessions.get(testData.get("username"));
+
+		//get the sending account id
+		int sendAccountId = customerAccounts.get( testData.get("to-account"));
+		//get the source account id
+		int receiveAccountId = customerAccounts.get(testData.get("from-account"));
+
+
 		//get the send account balance to check the funds before/after transfer
-		double sendAccountBalanceBefore 	= AccountsEndpoint.getBalanceById(sendAccountId, sessionId);		
+		double sendAccountBalanceBefore 	= AccountsEndpoint.getBalanceById(sendAccountId, sessionId);
 		//get the source account balance to check the funds before/after transfer
 		double receiveAccountBalanceBefore 	= AccountsEndpoint.getBalanceById(receiveAccountId, sessionId);
-		
-				
-		
+
+
+
 		double transferAmount 			= Double.parseDouble(testData.get("amount"));
 		double expectedTransferAmount 	= Double.parseDouble(testData.get("expected-transfered-amount"));
-		
-		
-		// perform the transfer and assert response code, perform response regex assertion, 
+
+
+		// perform the transfer and assert response code, perform response regex assertion,
 		TransferEndpoints.transfereBetweenAccounts(sendAccountId, receiveAccountId, transferAmount, sessionId)
 			.then()
 				.body(Matchers.matchesRegex("Successfully transferred \\$.* from account #" + sendAccountId + " to account #" + receiveAccountId)) // assert regex response body
 				.assertThat().statusLine(FrameworkConstants.HTTP_STATUS_OK) // assert status line
 				.assertThat().statusCode(200)// assert status code
 				.assertThat().time(Matchers.lessThan( REQUEST_ALLOWED_TIME_MS)); //assert time taken
-		
 
-		
+
+
 		// Get the updated accounts after the transfer
 		double receiveAccountBalanceAfter = AccountsEndpoint.getBalanceById(receiveAccountId, sessionId);
 		double SendAccountBalanceAfter = AccountsEndpoint.getBalanceById(sendAccountId, sessionId);
 
-		
+
 		// Verify the amount was deducted from the original account
 		Assert.assertEquals(SendAccountBalanceAfter,(sendAccountBalanceBefore - expectedTransferAmount),
 								"Verify that the amount was deducted from the sending account, Original balance "
 								+ sendAccountBalanceBefore + ", new balance "
 								+ SendAccountBalanceAfter);
 
-		// The balance of the receiving account before the transfer + expected transfer amount must equal the updated balance in the receiving account   
+		// The balance of the receiving account before the transfer + expected transfer amount must equal the updated balance in the receiving account
 		Assert.assertEquals((receiveAccountBalanceBefore + expectedTransferAmount),receiveAccountBalanceAfter,
 						"Verify that the correct amount was credited to the receiving account, old balance "
 						+ receiveAccountBalanceBefore + " - new Balance "
 						+ receiveAccountBalanceAfter);
-		
-		
+
+
 
 	}
 
@@ -111,17 +111,9 @@ public class TestTransfer extends BaseTest {
 
 
 
-	/**
-	 * generates an account key for the account in the format username.account-alias
-	 * @param testData
-	 * @return
-	 */
-	private static String getAccountKeyFromTestData(Map<String, String> testData) {
-		return testData.get("username")+"." + testData.get("from-account");
-	}
-	
-	
-	
-	
+
+
+
+
 
 }
