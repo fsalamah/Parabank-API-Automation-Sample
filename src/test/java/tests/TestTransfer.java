3edec74e275cfefb59a9d9eee65306611cc523ce
FaisalSalamah <faisal.salamah@live.com>
2024-04-23 00:53:10 +0400

    formatting

--- src/test/java/tests/TestTransfer.java
+++ src/test/java/tests/TestTransfer.java
@@ -28,92 +28,70 @@ public class TestTransfer extends BaseTest {
 	@BeforeClass
 	public void Setup() throws IOException {
 
-		// Clean up the database
-    Allure.step("Clean up the system db",()->AdminEndpoints.cleanDatabase());
+        // Clean up the database
+        Allure.step("Clean up the system db",()->AdminEndpoints.cleanDatabase());
 
-		// initialize account balances since the test relies on these
-		Allure.step("initialize account balances since the test relies on these",()-> AdminEndpoints.initializeAccountBalanceSettings());
+        // initialize account balances since the test relies on these
+        Allure.step("initialize account balances since the test relies on these",()-> AdminEndpoints.initializeAccountBalanceSettings());
 
-		//Setup the data for the tests (users and accounts)
-		Allure.step("Setup the data for the tests (users and accounts)" ,()->TestDataHelper.setupInitialTestData());
+        //Setup the data for the tests (users and accounts)
+        Allure.step("Setup the data for the tests (users and accounts)" ,()->TestDataHelper.setupInitialTestData());
 
 	}
 
 
-
-
-
-	@Test( testName = "Verify Transfer Positive Cases",
-			dataProvider 	= "test-data-provider",
-			dataProviderClass = TestDataProvider.class,
-			priority = 0, groups = {"regression", "smoke"})
+	@Test(  testName = "Verify Transfer Positive Cases",
+          dataProvider 	= "test-data-provider",
+          dataProviderClass = TestDataProvider.class,
+          priority = 0, groups = {"regression", "smoke"})
 	public void verifyTransferPositiveCases(Map<String, String> testData) {
 
-    AllureLifecycle lifecycle = Allure.getLifecycle();
-    lifecycle.updateTestCase(testResult -> testResult.setName(testData.get("description")));
+        AllureLifecycle lifecycle = Allure.getLifecycle();
+        lifecycle.updateTestCase(testResult -> testResult.setName(testData.get("description")));
 
-		//get the user token
-		var sessionId = Allure.step("get the user token " + testData.get("username"),
-                                ()-> userSessions.get(testData.get("username")));
+        //get the user token
+        var sessionId        = Allure.step("get the user token " + testData.get("username"),
+                                            ()-> userSessions.get(testData.get("username")));
 
-		//get the sending account id
-		int sendAccountId =  Allure.step("get the sending account id " ,
-                                    ()->customerAccounts.get( testData.get("from-account")));
-		//get the receiving account id
-		int receiveAccountId = Allure.step("get the receiving account id",
-                                      ()->customerAccounts.get(testData.get("to-account")));
+        //get the sending account id
+        int sendAccountId    = Allure.step("get the sending account id " ,
+                                            ()->customerAccounts.get( testData.get("from-account")));
+        //get the receiving account id
+        int receiveAccountId = Allure.step("get the receiving account id",
+                                            ()->customerAccounts.get(testData.get("to-account")));
 
+        //get the send account balance to check the funds before/after transfer
+        double sendAccountBalanceBefore = Allure.step("get the sending account balance before the transaction",
+                                                      ()->  AccountsEndpoint.getBalanceById(sendAccountId, sessionId));
 
-		//get the send account balance to check the funds before/after transfer
-    double sendAccountBalanceBefore = Allure.step("get the sending account balance before the transaction",
-                                                    ()->  AccountsEndpoint.getBalanceById(sendAccountId, sessionId));
-		//get the source account balance to check the funds before/after transfer
+        //get the source account balance to check the funds before/after transfer
+        double receiveAccountBalanceBefore 	= Allure.step("get the receiving account balance before the transaction",
+                                                          ()->AccountsEndpoint.getBalanceById(receiveAccountId, sessionId));
 
-		double receiveAccountBalanceBefore 	= Allure.step("get the receiving account balance before the transaction",
-                                                      ()->AccountsEndpoint.getBalanceById(receiveAccountId, sessionId));
+        double transferAmount 			    = Double.parseDouble(testData.get("amount"));
+        double expectedTransferAmount 	= Double.parseDouble(testData.get("expected-transfered-amount"));
 
+        // perform the transfer and assert response code, perform response regex assertion,
+        Allure.step("Execute the transfer from account " + sendAccountId + " to " + receiveAccountId + " with the amount " + transferAmount,
+                    ()->TransferEndpoints.transfereBetweenAccounts(sendAccountId, receiveAccountId, transferAmount, sessionId)
+                      .then()
+                      .body(Matchers.equalTo("Successfully transferred $"+expectedTransferAmount+" from account #" + sendAccountId + " to account #" + receiveAccountId)) // assert response body correctness
+                      .assertThat().statusLine(FrameworkConstants.HTTP_STATUS_OK) // assert status line
+                      .assertThat().statusCode(200)// assert status code
+                      .assertThat().time(Matchers.lessThan( REQUEST_ALLOWED_TIME_MS))); //assert time taken
 
-		double transferAmount 			    = Double.parseDouble(testData.get("amount"));
-		double expectedTransferAmount 	= Double.parseDouble(testData.get("expected-transfered-amount"));
+        // Get the updated accounts after the transfer
+        double receiveAccountBalanceAfter = Allure.step("get the receiving account balance after the transaction",
+                                                        ()-> AccountsEndpoint.getBalanceById(receiveAccountId, sessionId));
+        double SendAccountBalanceAfter =    Allure.step("get the sending account balance after the transaction",
+                                                        ()-> AccountsEndpoint.getBalanceById(sendAccountId, sessionId));
 
+        // Verify the amount was deducted from the original account
+        Allure.step("Verify that the amount was deducted from the sending account, Original balance "+ sendAccountBalanceBefore + ", new balance "+ SendAccountBalanceAfter,
+                    ()->Assert.assertEquals(SendAccountBalanceAfter,(sendAccountBalanceBefore - expectedTransferAmount)));
 
-		// perform the transfer and assert response code, perform response regex assertion,
-    Allure.step("Execute the transfer from account " + sendAccountId + " to " + receiveAccountId + " with the amount " + transferAmount,
-                  ()->TransferEndpoints.transfereBetweenAccounts(sendAccountId, receiveAccountId, transferAmount, sessionId)
-                  .then()
-                  .body(Matchers.equalTo("Successfully transferred $"+expectedTransferAmount+" from account #" + sendAccountId + " to account #" + receiveAccountId)) // assert response body correctness
-                  .assertThat().statusLine(FrameworkConstants.HTTP_STATUS_OK) // assert status line
-                  .assertThat().statusCode(200)// assert status code
-                  .assertThat().time(Matchers.lessThan( REQUEST_ALLOWED_TIME_MS))); //assert time taken
-
-
-
-		// Get the updated accounts after the transfer
-    double receiveAccountBalanceAfter = Allure.step("get the receiving account balance after the transaction",
-                                                    ()-> AccountsEndpoint.getBalanceById(receiveAccountId, sessionId));
-    double SendAccountBalanceAfter =    Allure.step("get the sending account balance after the transaction",
-                                                    ()-> AccountsEndpoint.getBalanceById(sendAccountId, sessionId));
-
-
-		// Verify the amount was deducted from the original account
-    Allure.step("Verify that the amount was deducted from the sending account, Original balance "+ sendAccountBalanceBefore + ", new balance "+ SendAccountBalanceAfter,
-                ()->Assert.assertEquals(SendAccountBalanceAfter,(sendAccountBalanceBefore - expectedTransferAmount)));
-
-		// The balance of the receiving account before the transfer + expected transfer amount must equal the updated balance in the receiving account
-    Allure.step("Verify that the correct amount was credited to the receiving account, old balance "+ receiveAccountBalanceBefore + " - new Balance "+ receiveAccountBalanceAfter,
-                ()->Assert.assertEquals((receiveAccountBalanceBefore + expectedTransferAmount),receiveAccountBalanceAfter));
-
-
-
+        // The balance of the receiving account before the transfer + expected transfer amount must equal the updated balance in the receiving account
+        Allure.step("Verify that the correct amount was credited to the receiving account, old balance "+ receiveAccountBalanceBefore + " - new Balance "+ receiveAccountBalanceAfter,
+                    ()->Assert.assertEquals((receiveAccountBalanceBefore + expectedTransferAmount),receiveAccountBalanceAfter));
 	}
-
-
-
-
-
-
-
-
-
-
 }
